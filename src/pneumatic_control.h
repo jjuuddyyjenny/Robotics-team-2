c0c3c9dbd9fe2c720cacca25256944a980098ab8
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-11-27 21:33:26 +0800

    Fix some trivial warnings

--- src/pneumatic_control.h
+++ src/pneumatic_control.h
@@ -1,6 +1,10 @@
+#ifndef __PNEUMATIC_CONTROL_H__
+#define __PNEUMATIC_CONTROL_H__
+
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
 
-
 void pneumatic_init();
-void pneumatic_control(GPIO_TypeDef* PORT,u16 gpio_pin,u8 state);
+void pneumatic_control(GPIO_TypeDef* PORT, u16 gpio_pin, u8 state);
+
+#endif
