b5708e5b8f90bb61eddbb2e5cdd7274fdc22ed98
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-11-27 22:23:56 +0800

    Add the header guard

--- src/motor.h
+++ src/motor.h
@@ -1,17 +1,12 @@
+#ifndef __MOTOR_H__
+#define __MOTOR_H__
+
 #include "stm32f10x.h"
-#include "stm32f10x_gpio.h"	  
+#include "stm32f10x_gpio.h"
 #include "stm32f10x_tim.h"
 #include "tft_display.h"
 
-
-
-
-
-
-
-
 void motor_init(void);
-void motor_control(u8 PWMx,u8 dir,u16 magnitude);//dir can be 1 or 0 , pwm can be 1 or 2  
+void motor_control(u8 PWMx,u8 dir,u16 magnitude); // dir can be 1 or 0 , pwm can be 1 or 2
 
-
-
+#endif
